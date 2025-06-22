cfad794b25b9668b4db550d055e6770c369ef4be
RaghadHabaybeh <rhabayba@gmail.com>
2025-06-23 01:07:38 +0300

    add query selecter

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -3,3 +3,8 @@ let currentFilter = "all";
 let deleteId = null;
 let actionType = null;
 let renameId = null;
+
+document.querySelector("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  saveTask();
+});
